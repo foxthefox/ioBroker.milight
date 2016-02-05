8ff5bac8d488d5f5708d60c4b23067e38eb63626
foxthefox <github@wysiwis.net>
2016-02-05 22:14:08 +0100

    Update milight.js

--- milight.js
+++ milight.js
@@ -12,7 +12,7 @@ var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
 // you have to call the adapter function and pass a options object
 // name has to be set and has to be equal to adapters folder name and main file name excluding extension
 // adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.template.0
-var adapter = utils.adapter('jeelink');
+var adapter = utils.adapter('milight');
 
 // is called when adapter shuts down - callback has to be called under any circumstances!
 adapter.on('unload', function (callback) {
@@ -64,8 +64,8 @@ function main() {
 
     // The adapters config (in the instance object everything under the attribute "native") is accessible via
     // adapter.config:
-    adapter.log.info('config test1: ' + adapter.config.test1);
-    adapter.log.info('config test1: ' + adapter.config.test2);
+    adapter.log.info('config test1: ' + adapter.config.group.1.room);
+    adapter.log.info('config test2: ' + adapter.config.group.2.ltype);
 
     var options = {
         bridge:     adapter.config.milight-ip       || '192.168.178.48',
