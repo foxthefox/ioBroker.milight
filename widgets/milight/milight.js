ac7a5a949db634e1668a9ac1249c6f3ea1b65156
foxthefox <github@wysiwis.net>
2017-01-08 11:21:39 +0100

    Create milight.js

---
+++ widgets/milight/milight.js
@@ -0,0 +1,57 @@
+/*
+    ioBroker.milight Widget-Set
+    version: "0.0.1"
+    Copyright foxthefox 
+    adapted from iobroker.vis-jqui, jqui-mfd
+
+*/
+
+"use strict";
+
+
+
+// add translations for edit mode
+
+if (vis.editMode) {
+
+    $.extend(systemDictionary, {
+        "oid-level":       {"en": "brightness", "de": "Helligkeit"},
+        "oid-colormode":    {"en": "mode",  "de": "Modus"},
+        "oid-color":        {"en": "color",      "de": "Farbe"},
+        "oid-disco":        {"en": "disco mode",      "de": "Disco"},
+        "oid-speedup":      {"en": "speed up",      "de": "speed up"},
+        "oid-speeddown":    {"en": "speed down",      "de": "speed down"}        
+    });
+};
+
+vis.binds.milightui = {
+
+    version: "0.0.1",
+
+    showVersion: function() {
+
+        if (vis.binds.milightui.version) {
+            console.log('Milight widget version: ' + vis.binds.milightui.version);
+            vis.binds.milightui.version = null;
+        }
+    },
+
+    milightColormode: function (el, oid) {
+            var $hue = $(el).parent().find('.hue-mode-hue');
+            if (vis.states.attr(oid + '.val') == 'color') {
+                $hue.hide();
+            } else {
+                $hue.show();
+            }
+
+            vis.states.bind(oid + '.val', function (e, newVal, oldVal) {
+                if (newVal == 'color') {
+                    $hue.hide();
+                } else {
+                    $hue.show();
+                }
+            });
+        }
+    };
+
+vis.binds.milightui.showVersion();
