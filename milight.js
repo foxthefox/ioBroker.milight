c6757902eaced37a2922983ee0c39606fca5d287
foxthefox <github@wysiwis.net>
2017-02-04 15:29:30 +0100

    boolean for full bright

--- milight.js
+++ milight.js
@@ -201,7 +201,7 @@ adapter.on('stateChange', function (id, state) {
             if (dp === 'state') {
                 if (state.val === 'true' || state.val === true || state.val === 1 || state.val === 'on' || state.val === 'ON') {
                     adapter.log.debug('Send to zone ' + zone + ' ON');
-                    if (adapter.config.v5onFullBright === 1){
+                    if (adapter.config.v5onFullBright){
                         light.sendCommands(zones[zone].on(zone), zones[zone].brightness(100), zones[zone].whiteMode(zone)).then(function () {
                             adapter.setForeignState(id, true, true);
                         }, function (err) {
