c75796d3b18407af15e72222d347a6c35a09f6f9
foxthefox <github@wysiwis.net>
2017-07-08 16:43:08 +0200

    add buttonToggle and state

--- widgets/milight/js/milight.js
+++ widgets/milight/js/milight.js
@@ -1,6 +1,6 @@
 /*
     ioBroker.milight Widget-Set
-    version: "0.0.1"
+    version: "0.0.2"
     Copyright foxthefox 
     adapted from iobroker.vis-jqui, jqui-mfd
 
@@ -25,7 +25,7 @@ if (vis.editMode) {
 
 vis.binds.milightui = {
 
-    version: "0.0.1",
+    version: "0.0.2",
 
     showVersion: function() {
 
@@ -34,7 +34,57 @@ vis.binds.milightui = {
             vis.binds.milightui.version = null;
         }
     },
+    //; vis.binds.musiccast.buttonToggle(el, data.value)
+    buttonToggle: function (el, val) {
+        var $this = $(el);
+        var oid = $this.attr('data-oid');
 
+        if (!$(el).data('no-style')) {
+            setTimeout(function () {
+                $(el).button();
+            }, 0);
+        }
+        $(el).click(function () {
+            if (val === undefined || val === null) val = false;
+            if (val === 'true')  val = true;
+            if (val === 'false') val = false;
+            if (parseFloat(val).toString() == val) val = parseFloat(val);
+            if (!vis.editMode) vis.setValue(oid, val);
+        });
+    },
+    state: function (el, oid) {
+        var $this = $(el);
+        var oid = (oid ? oid : $this.attr('data-oid'));
+        var val = $this.attr('data-val');
+
+        if (oid) $this.attr('data-oid', oid);
+
+        if (val === 'true')  val = true;
+        if (val === 'false') val = false;
+
+        if (!vis.editMode) {
+            $this.on('click touchstart', function (e) {
+                // Protect against two events
+                if (vis.detectBounce(this)) return;
+
+                var oid = $(this).attr('data-oid');
+
+                if ($(this).attr('url-value')) {
+                    vis.conn.httpGet($(this).attr('url-value'));
+                }
+
+                if (oid) {
+                    var val = $(this).attr('data-val');
+                    if (val === undefined || val === null) val = false;
+                    if (val === 'true')  val = true;
+                    if (val === 'false') val = false;
+                    if (parseFloat(val).toString() == val) val = parseFloat(val);
+
+                    if (oid) vis.setValue(oid, val);
+                }
+            });
+        }
+    },
     milightColormode: function (el, oid) {
             var $hue = $(el).parent().find('.hue-mode-hue');
             if (vis.states.attr(oid + '.val') === 'ct') {
