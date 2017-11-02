d8bb19fcc5af7985ea45d4ab082aabe70197dc92
foxthefox <github@wysiwis.net>
2017-11-03 00:23:30 +0100

    Update milight.js

--- widgets/milight/js/milight.js
+++ widgets/milight/js/milight.js
@@ -255,7 +255,7 @@ vis.binds.milightui = {
             }
         });
     },
-    hueslider1: function (el, options) {
+    rgbslider: function (el, options) {
         var $this = $(el);
         var oid = $this.attr('data-oid');
         var oid_val = 0;
