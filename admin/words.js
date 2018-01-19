c08af2bcf321c84c0707b989c55cd2510de28a7d
foxthefox <github@wysiwis.net>
2018-01-19 23:27:16 +0100

    adminv3

---
+++ admin/words.js
@@ -0,0 +1,21 @@
+    // Dictionary (systemDictionary is global variable from adapter-settings.js)
+    systemDictionary = {
+        "MiLight adapter settings": {"de": "MiLight Adapter Einstellung",       "ru": "Настройки MiLight Драйвера"},
+        "Select one":       {"en": "Select one",    "de": "eins auswählen",     "ru": "выберите"},
+        "MiLight IP":       {"en": "MiLight IP",    "de": "MiLight IP",         "ru": "MiLight IP"},
+        "Version":          {"en": "Version",       "de": "Version",            "ru": "Версия"},
+        "MiLight Port":     {"en": "MiLight Port",  "de": "MiLight Port",       "ru": "MiLight порт"},
+        "Command repeat":   {"en": "Command repeat", "de": "Wiederholen (mal)", "ru": "Повтор команды (раз)"},
+        "Zone 1":           {"en": "Zone 1",        "de": "Zone 1",             "ru": "Зона 1"},
+        "base":             {"en": "base",          "de": "basis",              "ru": "базовая"},
+	"White":            {"en": "White",         "de": "weiß",               "ru": ""},
+        "RGB":              {"en": "RGB",           "de": "RGB",                "ru": "RGB"},
+        "RGBW":             {"en": "RGBW",          "de": "RGBW",               "ru": "RGBW"},
+        "RGBWW/Dimmer":     {"en": "RGBWW/Dimmer",  "de": "RGBWW/Dimmer",       "ru": "RGBWW/диммер"},
+        "Zone 2":           {"en": "Zone 2",        "de": "Zone 2",             "ru": "Зона 2"},
+        "Zone 3":           {"en": "Zone 3",        "de": "Zone 3",             "ru": "Зона 3"},
+        "Zone 4":           {"en": "Zone 4",        "de": "Zone 4",             "ru": "Зона 4"},
+	"delayBetweenCommands": {"en":" delayBetweenCommands", "de":"Zeit zwischen Kommandos", "ru":""},   
+        "Nothing found":    {"en": "Nothing found", "de": "Nichts gefunden",    "ru": "Ничего не найдено"},
+	 "v5 On Full Brightness and white": {"en": "when switched on, set full brightness and white?", "de": "beim Einschalten volle Helligkeit und weiß setzen?", "ru": "white full brightness"}
+    }
